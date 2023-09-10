ab0ce04f34483d7c900d9ab87e755e8da1d468ea
ubuntu machine <abp595@yahoo.com>
2023-09-10 11:15:23 -0400

    add return type for planEntry view

--- app/Livewire/PlanEntry.php
+++ app/Livewire/PlanEntry.php
@@ -1,14 +1,13 @@
 <?php
- 
+
 namespace App\Livewire;
- 
-use Livewire\Component;
+
 use App\Models\Plan;
- 
+use Livewire\Component;
+
 class PlanEntry extends Component
 {
-
-    public function render()
+    public function render(): View
     {
         return view('livewire.plan-entry', [
             'plan' => Plan::all()->first(),
